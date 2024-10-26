56f4139f829f4a8cd253a2579b8b9f413e8f7de4
Li Keny Rodríguez Domínguez <74258644+keni2002@users.noreply.github.com>
2024-10-26 08:06:02 -0400

    Delete InventorySystem.Models/ViewModels directory

--- InventorySystem.Models/ViewModels/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace InventorySystem.Models.ViewModels;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
